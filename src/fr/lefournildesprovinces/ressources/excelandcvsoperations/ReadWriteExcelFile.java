2afb393b727e34482d4053ff8dce30155d84338b
K4M1coder <kamicth@gmail.com>
2015-08-18 03:32:47 +0200

    suppress warning on xls features tests

--- src/fr/lefournildesprovinces/ressources/excelandcvsoperations/ReadWriteExcelFile.java
+++ src/fr/lefournildesprovinces/ressources/excelandcvsoperations/ReadWriteExcelFile.java
@@ -20,23 +20,24 @@ public class ReadWriteExcelFile {
 	public static void readXLSFile() throws IOException
 	{
 		InputStream ExcelFileToRead = new FileInputStream("C:/Test.xls");
+		@SuppressWarnings("resource")
 		HSSFWorkbook wb = new HSSFWorkbook(ExcelFileToRead);
 
 		HSSFSheet sheet=wb.getSheetAt(0);
-		HSSFRow row; 
+		HSSFRow row;
 		HSSFCell cell;
 
-		Iterator rows = sheet.rowIterator();
+		Iterator<?> rows = sheet.rowIterator();
 
 		while (rows.hasNext())
 		{
 			row=(HSSFRow) rows.next();
-			Iterator cells = row.cellIterator();
-			
+			Iterator<?> cells = row.cellIterator();
+
 			while (cells.hasNext())
 			{
 				cell=(HSSFCell) cells.next();
-		
+
 				if (cell.getCellType() == HSSFCell.CELL_TYPE_STRING)
 				{
 					System.out.print(cell.getStringCellValue()+" ");
@@ -47,16 +48,17 @@ public class ReadWriteExcelFile {
 				}
 				else
 				{
-					//U Can Handel Boolean, Formula, Errors
+					//U Can Handle Boolean, Formula, Errors
 				}
 			}
 			System.out.println();
 		}
-	
+
 	}
-	
+
+	@SuppressWarnings("resource")
 	public static void writeXLSFile() throws IOException {
-		
+
 		String excelFileName = "C:/Test.xls";//name of excel file
 
 		String sheetName = "Sheet1";//name of sheet
@@ -68,45 +70,46 @@ public class ReadWriteExcelFile {
 		for (int r=0;r < 5; r++ )
 		{
 			HSSFRow row = sheet.createRow(r);
-	
+
 			//iterating c number of columns
 			for (int c=0;c < 5; c++ )
 			{
 				HSSFCell cell = row.createCell(c);
-				
+
 				cell.setCellValue("Cell "+r+" "+c);
 			}
 		}
-		
+
 		FileOutputStream fileOut = new FileOutputStream(excelFileName);
-		
+
 		//write this workbook to an Outputstream.
 		wb.write(fileOut);
 		fileOut.flush();
 		fileOut.close();
 	}
-	
+
+	@SuppressWarnings({ "resource", "unused" })
 	public static void readXLSXFile() throws IOException
 	{
 		InputStream ExcelFileToRead = new FileInputStream("C:/Test.xlsx");
 		XSSFWorkbook  wb = new XSSFWorkbook(ExcelFileToRead);
-		
-		XSSFWorkbook test = new XSSFWorkbook(); 
-		
+
+		XSSFWorkbook test = new XSSFWorkbook();
+
 		XSSFSheet sheet = wb.getSheetAt(0);
-		XSSFRow row; 
+		XSSFRow row;
 		XSSFCell cell;
 
-		Iterator rows = sheet.rowIterator();
+		Iterator<?> rows = sheet.rowIterator();
 
 		while (rows.hasNext())
 		{
 			row=(XSSFRow) rows.next();
-			Iterator cells = row.cellIterator();
+			Iterator<?> cells = row.cellIterator();
 			while (cells.hasNext())
 			{
 				cell=(XSSFCell) cells.next();
-		
+
 				if (cell.getCellType() == XSSFCell.CELL_TYPE_STRING)
 				{
 					System.out.print(cell.getStringCellValue()+" ");
@@ -122,11 +125,12 @@ public class ReadWriteExcelFile {
 			}
 			System.out.println();
 		}
-	
+
 	}
-	
+
+	@SuppressWarnings("resource")
 	public static void writeXLSXFile() throws IOException {
-		
+
 		String excelFileName = "C:/Test.xlsx";//name of excel file
 
 		String sheetName = "Sheet1";//name of sheet
@@ -143,7 +147,7 @@ public class ReadWriteExcelFile {
 			for (int c=0;c < 5; c++ )
 			{
 				XSSFCell cell = row.createCell(c);
-	
+
 				cell.setCellValue("Cell "+r+" "+c);
 			}
 		}
@@ -157,10 +161,10 @@ public class ReadWriteExcelFile {
 	}
 
 	public static void main(String[] args) throws IOException {
-		
+
 		writeXLSFile();
 		readXLSFile();
-		
+
 		writeXLSXFile();
 		readXLSXFile();
 
